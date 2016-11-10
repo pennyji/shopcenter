89c69d0ca417f68efb7acb1e4d75450bfceae30a
156906432 <alone0927@163.com>
2016-11-10 16:56:35 +0800

    ..

--- src/main/java/com/cheer/mini/ums/dao/UserDao.java
+++ src/main/java/com/cheer/mini/ums/dao/UserDao.java
@@ -8,23 +8,12 @@ public interface UserDao {
 
     public User getByAccount(String account);
 
-    public User get(String id);
-
     public User login(String account, String password);
 
     public int save(User user);
     
-    public List<User> getUserAll();
-    
-
     public List<User> queryAll();
     
     public void update(User user);
-
-
-    public List<User> queryAll();
-    
-    public void update(User user);
-
 
 }
