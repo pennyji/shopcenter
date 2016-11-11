0096c99b52e39dc6507f5fc912485fcfa3842c31
1042545965 <1042545965@qq.com>
2016-11-11 16:18:56 +0800

    修改了pms

--- src/main/java/com/cheer/mini/pms/service/ProductService.java
+++ src/main/java/com/cheer/mini/pms/service/ProductService.java
@@ -1,9 +1,13 @@
 package com.cheer.mini.pms.service;
 
+import java.util.List;
+
 import com.cheer.mini.pms.model.Product;
 
 public interface ProductService {
+	
+		//查找热门商品
+	public List<Product> findByHot(int hot);
 
-	public Product getProductId(String productId);
 	
 }
