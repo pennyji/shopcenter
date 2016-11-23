c3bef26a9a167422ff31d973fbfa41d75dd3aaa3
1042545965 <1042545965@qq.com>
2016-11-23 10:33:46 +0800

    02

--- src/main/java/com/cheer/mini/ums/controller/ValidateAjaxController.java
+++ src/main/java/com/cheer/mini/ums/controller/ValidateAjaxController.java
@@ -1,6 +1,7 @@
 package com.cheer.mini.ums.controller;
 
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
